fe65ad89b2c3beae52a9babcdfad58a7162cf657
Engel Noel Berihuete <emaxing23@gmail.com>
2025-05-22 15:57:35 -0400

    adding models

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+  nombre = models.CharField(max_length=60)
+
+class Producto(models.Model):
+  nombre = models.CharField(max_length=60)
+  stock = models.IntegerField()
+  puntaje = models.FloatField()
+  # CASCADE : eliminar el producto
+  # PROTECT : lanza un error 
+  # RESTRICT : solo elimina si no existe productos
+  # SET_null : Actuliza valor nulo
+  # SET_DEFAULT:
+  Categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+  
