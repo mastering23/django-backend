9c7975d33b5bb57a2ea96f8d072b7d22f39395e9
Engel Noel Berihuete <emaxing23@gmail.com>
2025-05-23 08:12:43 -0400

    get method and testing jsonresponse

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria,Producto
 
 # Register your models here.
+class CategoriaAdmin(admin.ModelAdmin):
+  list_display =('id','nombre')
+  
+admin.site.register(Categoria,CategoriaAdmin)
+admin.site.register(Producto)
